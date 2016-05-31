6ae2feb3d8ad076f48166ab0adabdfd20b2d5a7c
Hernan Gonzalo Diaz Ailan <Gonzalo_Devlain@hotmail.com>
2016-05-31 10:25:33 -0300

    how page

--- js/app.routes.js
+++ js/app.routes.js
@@ -10,6 +10,48 @@
                       private: false
                     }
 			})
+			.when('/como-funciona', {
+				templateUrl: 'views/how.html',
+				controller: 'HomeCtrl',
+				data: {
+					private: false
+				}
+			})
+			.when('/preguntas-frecuentes', {
+				templateUrl: 'views/faqs.html',
+				controller: 'HomeCtrl',
+				data: {
+					private: false
+				}
+			})
+			.when('/sobre-nosotros', {
+				templateUrl: 'views/about.html',
+				controller: 'HomeCtrl',
+				data: {
+					private: false
+				}
+			})
+			.when('/partners', {
+				templateUrl: 'views/partners.html',
+				controller: 'HomeCtrl',
+				data: {
+					private: false
+				}
+			})
+			.when('/plataforma', {
+				templateUrl: 'views/platform.html',
+				controller: 'HomeCtrl',
+				data: {
+					private: false
+				}
+			})
+			.when('/contacto', {
+				templateUrl: 'views/contact.html',
+				controller: 'HomeCtrl',
+				data: {
+					private: false
+				}
+			})
 			.otherwise({
 				redirectTo: '/'
 			})
